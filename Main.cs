822418259533995da11159d8e5b4c19a6904da80
SmallJoker <mk939@ymail.com>
2015-01-04 11:33:49 +0100

    Update colors.bmp + Function to draw all blocks + Better error messages - Remove colors2.bmp (colors.bmp contains everything)

--- Main.cs
+++ Main.cs
@@ -10,11 +10,11 @@ namespace BB_Artist {
 	public partial class Main : Form {
 		Client cli;
 		Connection con;
-		byte[][] FG, BG;
+		byte[][][] colors;
 		Random rd;
 		int[][][] bl;
 		string key;
-		int wX, wY, delay = 10;
+		int worldWidth, worldHeight, delay = 10;
 		bool running, isOwner;
 
 		public Main() {
@@ -26,27 +26,23 @@ namespace BB_Artist {
 			rd = new Random((int)DateTime.Now.Ticks);
 			b_draw.Enabled = false;
 			gb_settings.Enabled = false;
-			if (File.Exists("colors.bmp") && File.Exists("colors2.bmp")) {
-				FileStream f = new FileStream("colors.bmp", FileMode.Open);
-				Bitmap t = new Bitmap(f);
-				FG = new byte[t.Width][];
-				for (int i = 0; i < t.Width; i++) {
-					Color v = t.GetPixel(i, 0);
-					FG[i] = new byte[] { v.A, v.R, v.G, v.B };
-				}
-				f.Close();
-				f = new FileStream("colors2.bmp", FileMode.Open);
-				t = new Bitmap(f);
-				BG = new byte[t.Width][];
-				for (int i = 0; i < t.Width; i++) {
-					Color v = t.GetPixel(i, 0);
-					BG[i] = new byte[] { v.A, v.R, v.G, v.B };
-				}
-				f.Close();
-			} else {
-				MessageBox.Show("Error: Can not load \"colors.bmp\" or \"colors2.bmp\"");
+			if (!File.Exists("colors.bmp")) {
+				MessageBox.Show("Error: Can not load \"colors.bmp\"");
 				Close();
+				return;
 			}
+			FileStream f = new FileStream("colors.bmp", FileMode.Open);
+			Bitmap t = new Bitmap(f);
+			colors = new byte[t.Height][][];
+			
+			for (int y = 0; y < t.Height; y++) {
+				colors[y] = new byte[t.Width][];
+				for (int x = 0; x < t.Width; x++) {
+					Color v = t.GetPixel(x, y);
+					colors[y][x] = new byte[] { v.A, v.R, v.G, v.B };
+				}
+			}
+			f.Close();
 		}
 
 		void Main_Closing(object sender, FormClosingEventArgs e) {
@@ -65,44 +61,58 @@ namespace BB_Artist {
 			if (m.Type == "init") {
 				key = getkey(m.GetString(3));
 				isOwner = m.GetBoolean(9);
-				wX = m.GetInt(10);
-				wY = m.GetInt(11);
-				nu_xp.Maximum = wX;
-				nu_yp.Maximum = wY;
+				worldWidth = m.GetInt(10);
+				worldHeight = m.GetInt(11);
+				nu_xp.Maximum = worldWidth;
+				nu_yp.Maximum = worldHeight;
 				WD_parser(m, 13);
 				con.Send(key + 'f', 1);
 				con.Send("access", tb_code.Text);
-			} else if (m.Type == "access") {
+				return;
+			}
+			if (m.Type == "access") {
 				MessageBox.Show("Got Access.");
-			} else if (m.Type == "lostaccess") {
+				return;
+			}
+			if (m.Type == "lostaccess") {
 				if (!isOwner) {
 					running = false;
 					MessageBox.Show("Lost Access.");
 				}
-			} else if (m.Type == "clear") {
-				wX = m.GetInt(0);
-				wY = m.GetInt(1);
+				return;
+			}
+			if (m.Type == "clear") {
+				worldWidth = m.GetInt(0);
+				worldHeight = m.GetInt(1);
 				bl = new int[2][][];
 				for (int l = 0; l < 2; l++) {
-					bl[l] = new int[wX][];
-					for (int x = 0; x < wX; x++) {
-						bl[l][x] = new int[wY];
+					bl[l] = new int[worldWidth][];
+					for (int x = 0; x < worldWidth; x++) {
+						bl[l][x] = new int[worldHeight];
 					}
 				}
-				for (int x = 0; x < wX; x++) {
+				for (int x = 0; x < worldWidth; x++) {
 					bl[0][x][0] = 9;
-					bl[0][x][wY - 1] = 9;
+					bl[0][x][worldHeight - 1] = 9;
 				}
-				for (int y = 1; y < wY - 1; y++) {
+				for (int y = 1; y < worldHeight - 1; y++) {
 					bl[0][0][y] = 9;
-					bl[0][wX - 1][y] = 9;
+					bl[0][worldWidth - 1][y] = 9;
 				}
-			} else if (m.Type == "reset") {
+				return;
+			}
+			if (m.Type == "reset") {
 				WD_parser(m, 0);
-			} else if (m.Type == "b" && bl != null) {
+				return;
+			}
+			if (bl == null) return;
+			if (m.Type == "b") {
 				bl[m.GetInt(0)][m.GetInt(1)][m.GetInt(2)] = m.GetInt(3);
-			} else if ((m.Type == "bc" || m.Type == "bs" || m.Type == "lb" || m.Type == "pt") && bl != null) {
+				return;
+			}
+			if (m.Type == "bc" || m.Type == "bs" || m.Type == "lb" || m.Type == "pt") {
 				bl[0][m.GetInt(0)][m.GetInt(1)] = m.GetInt(2);
+				return;
 			}
 		}
 
@@ -133,9 +143,9 @@ namespace BB_Artist {
 		void WD_parser(PlayerIOClient.Message m, uint c) {
 			bl = new int[2][][];
 			for (int l = 0; l < 2; l++) {
-				bl[l] = new int[wX][];
-				for (int x = 0; x < wX; x++) {
-					bl[l][x] = new int[wY];
+				bl[l] = new int[worldWidth][];
+				for (int x = 0; x < worldWidth; x++) {
+					bl[l][x] = new int[worldHeight];
 				}
 			}
 			while (c < m.Count) {
@@ -169,12 +179,12 @@ namespace BB_Artist {
 				try {
 					cli = PlayerIO.QuickConnect.SimpleConnect("eehw11-uzvrgq6urkyo6tdtrgm1ra", tb_mail.Text, tb_pass.Text);
 					isConnected = true;
-				} catch {
-					MessageBox.Show("Can not connect, check your login data.");
+				} catch(PlayerIOError err) {
+					MessageBox.Show(err.Message, "Logging in failed");
 				}
 				if (isConnected) {
 					try {
-						con = cli.Multiplayer.CreateJoinRoom(tb_wid.Text, "EE5", true, null, null);
+						con = cli.Multiplayer.JoinRoom(tb_wid.Text, null);
 						con.OnMessage += new MessageReceivedEventHandler(OnMsg);
 						con.OnDisconnect += delegate(object o, string s) {
 							running = false;
@@ -187,7 +197,7 @@ namespace BB_Artist {
 						b_con.Text = "Disconnect";
 						b_draw.Enabled = true;
 					} catch(PlayerIOError err) {
-						MessageBox.Show("Can not open or join the room." + err.Message);
+						MessageBox.Show(err.Message, "Joining world failed");
 					}
 				}
 			} else {
@@ -238,73 +248,95 @@ namespace BB_Artist {
 		}
 
 		void building() {
+			//draw_blocks(); return;
+
 			FileStream fb = new FileStream(ofd.FileName, FileMode.Open);
 			Bitmap bt = new Bitmap(fb);
 			fb.Close();
-			byte[] last_color = new byte[]{ 0, 0, 0, 0 };
-			int last_id = 0;
-			bool last_layer = true,
-				overr = true;
+			byte[] last_color = new byte[] { 0, 0, 0, 0 };
+			int last_id = 0,
+				last_layer = 0;
+
+			bool overr = true;
+
 			int aX = 0,
 				aY = 0;
+
 			if (rb_att.Checked) {
 				aX = (int)nu_xp.Value;
 				aY = (int)nu_yp.Value;
 				overr = false;
 			}
 			
-			for (int y = 0; y + aY < wY && y < bt.Height && running; y++) {
-				for (int x = 0; x + aX < wX && x < bt.Width && running; x++) {
+			for (int y = 0; y + aY < worldHeight && y < bt.Height && running; y++) {
+				for (int x = 0; x + aX < worldWidth && x < bt.Width && running; x++) {
 					Color f = bt.GetPixel(x, y);
 					byte[] c = new byte[]{ f.A, f.R, f.G, f.B };
-					int b = 0;
-					byte layer = 0;
+					int id = 0,
+						layer = 0;
 
 					if (last_color[0] != c[0] || 
 						last_color[1] != c[1] ||
 						last_color[2] != c[2] || 
 						last_color[3] != c[3]) {
-						int dif = 9999;
-						
-						for (int i = 0; i < FG.Length; i++) {
-							int d = difco(FG[i], c);
-							if (d < dif) {
-								dif = d;
-								b = i;
+						int diff = 0xFFFF;
+
+						for (int j = 0; j < colors.Length; j++) {
+							for (int k = 0; k < colors[j].Length; k++) {
+								int d = difco(colors[j][k], c);
+								if (d < diff) {
+									diff = d;
+									id = k;
+									if (j == 1) id += 400;
+									if (j == 2) {
+										id += 500;
+										layer = 1;
+									}
+								}
 							}
 						}
-						for (int i = 0; i < BG.Length; i++) {
-							int d = difco(BG[i], c);
-							if (d < dif) {
-								dif = d;
-								b = i + 500;
-								layer = 1;
-							}
-						}
-						last_id = b;
+
+						last_id = id;
 						last_layer = layer;
 						last_color = c;
 					} else {
-						b = last_id;
+						id = last_id;
 						layer = last_layer;
 					}
-					if (bl[layer][x + aX][y + aY] != b) {
-						con.Send(key, layer, x + aX, y + aY, b);
+					if (bl[layer][x + aX][y + aY] != id) {
+						con.Send(key, layer, x + aX, y + aY, id);
 						Thread.Sleep(delay);
 					}
 					
 					// Remove useless Backgrounds or Foreground blocks
-					byte other_layer = (layer == 0)? 1 : 0;
+					int other_layer = (layer == 0)? 1 : 0;
 					if (bl[other_layer][x + aX][y + aY] != 0) {
 						con.Send(key, other_layer, x + aX, y + aY, 0);
 						Thread.Sleep(delay);
 					}
 				}
 			}
-			try {
-				running = false;
-				b_draw.Text = "Draw";
-			} catch { }
+			running = false;
+			b_draw.Text = "Draw";
+		}
+
+		void draw_blocks() {
+			int aX = 0,
+				aY = 0;
+
+			if (rb_att.Checked) {
+				aX = (int)nu_xp.Value;
+				aY = (int)nu_yp.Value;
+			}
+
+			for (int id = 0; id < 100; id++) {
+				con.Send(key, 0, aX + id, aY, id);
+				Thread.Sleep(delay);
+				con.Send(key, 0, aX + id, aY + 1, id + 400);
+				Thread.Sleep(delay);
+				con.Send(key, 1, aX + id, aY + 2, id + 500);
+				Thread.Sleep(delay);
+			}
 		}
 
 		void rb_over_CheckedChanged(object sender, EventArgs e) {
